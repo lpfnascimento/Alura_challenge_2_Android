beaef255b68a4665b277160049bd4d5cbdd6ee9f
liliane.nascimento <liliane.nascimento@eldorado.org.br>
2021-10-07 12:56:52 -0300

    tela SplashScreen - handler não funcionando

--- app/src/main/java/alura/ecommerce/challenge/ui/MainActivity.kt
+++ app/src/main/java/alura/ecommerce/challenge/ui/MainActivity.kt
@@ -1,11 +1,13 @@
-package alura.ecommerce.challenge
+package alura.ecommerce.challenge.ui
 
+import alura.ecommerce.challenge.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
-}
+
+
+    }
